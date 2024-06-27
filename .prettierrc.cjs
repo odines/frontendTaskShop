9e252ef18cbe8d23cf6a581160fa57ec29e522e8
Denis Orlov <denis.orlov@digitalchief.ru>
2024-06-27 22:30:17 +0300

    Inital commit (created application with vue3+vite+pinia)

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  insertPragma: false,
+  endOfLine: 'auto',
+  HTMLWhitespaceSensitivity: 'ignore',
+  spaceBeforeFunctionParen: true
+};
